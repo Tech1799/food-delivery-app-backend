d097e6e560b17309d478fc29ceed17748d9f86c6
Tech1799 <bhupindersharma1799@gmail.com>
2021-12-04 07:52:04 +0545

    main

--- src/emails/signupOTP.js
+++ src/emails/signupOTP.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const verificationMail = (email, otp) => {
-    sgMail.send({
+    const message = {
         to: email,
         from: 'bhupindersharma1799@gmail.com',
         subject: 'OTP verification Mail',
@@ -11,7 +11,12 @@ const verificationMail = (email, otp) => {
             <h1>Your Email Verification code:</h1>
             <h3>${otp}</h3>
         `
-    })
+    }
+    return new Promise((resolve, reject) => {
+        sgMail.send(message, (error, info) =>
+          error ? reject(error) : resolve(info)
+        )
+      })
 }
 
 
